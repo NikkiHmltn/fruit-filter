1d00a01584b762c78d0a32707601e66c8534521b
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-10 08:41:49 -0800

    [List] Add class component

---
+++ src/components/List.js
@@ -0,0 +1,18 @@
+import React, {Components} from 'react'
+
+export default class List extends Components {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <div>
+                <h1>List</h1>
+            </div>
+        )
+    }
+}
+
